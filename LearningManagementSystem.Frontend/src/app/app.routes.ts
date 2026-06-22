fabdfbde5941bb4905398fa5c473feff2ae92db7
nandhiraja <nandhiraja16@gmail.com>
2026-06-22 14:30:42 +0530

    feat: add basic route paths

--- LearningManagementSystem.Frontend/src/app/app.routes.ts
+++ LearningManagementSystem.Frontend/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./features/landing/pages/landing/landing').then((m) => m.LandingComponent),
+    pathMatch: 'full'
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./features/auth/auth.routes').then((m) => m.authRoutes)
+  },
+  {
+    path: 'profile',
+    loadComponent: () => import('./features/profile/pages/profile/profile').then((m) => m.ProfileComponent),
+    canActivate: [authGuard]
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
