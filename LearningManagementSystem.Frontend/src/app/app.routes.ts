4b11430cb38597aa782598741dbbd674a18a70ca
nandhiraja <nandhiraja16@gmail.com>
2026-06-24 23:40:37 +0530

    feat: add landingpage route

--- LearningManagementSystem.Frontend/src/app/app.routes.ts
+++ LearningManagementSystem.Frontend/src/app/app.routes.ts
@@ -2,19 +2,14 @@ import { Routes } from '@angular/router';
 import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
-  {
+ {
     path: '',
-    loadComponent: () => import('./features/landing/pages/landing/landing').then((m) => m.LandingComponent),
+    loadComponent: () => import('./features/landing/landing.component').then((m) => m.LandingComponent),
     pathMatch: 'full'
   },
   {
     path: 'auth',
     loadChildren: () => import('./features/auth/auth.routes').then((m) => m.authRoutes)
-  },
-  {
-    path: 'profile',
-    loadComponent: () => import('./features/profile/pages/profile/profile').then((m) => m.ProfileComponent),
-    canActivate: [authGuard]
   },
   {
     path: '**',
